02378497bab34c08cd6a65d8754927c893ef2313
Digital House <digitalhouse@DH-A1-26.local>
2019-05-09 21:29:04 -0300

    Cria classe Pessoa

---
+++ Anderson/Anderson/Pessoa.swift
@@ -0,0 +1,14 @@
+//
+//  Pessoa.swift
+//  Anderson
+//
+//  Created by Digital House on 09/05/19.
+//  Copyright © 2019 Digital House. All rights reserved.
+//
+
+import Foundation
+
+class Pessoa {
+    var nome: String = "Anderson"
+    var idade: Int = 40
+}
